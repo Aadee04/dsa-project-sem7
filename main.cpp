b913b21cc27e20cb72f6e456b8a5524456b3b029
Aadee Sawarkar <aadee2004@gmail.com>
2023-10-31 13:18:57 +0530

    Polished parts of the code
    Probably the final version

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <queue>
 #include <unordered_map>
 #include <vector>
-#include <filesystem>
 
 using namespace std;
 
@@ -25,15 +24,13 @@ struct NodeComparator {
     }
 };
 
-
-// Build Huffman tree
 HuffmanNode* buildHuffmanTree(const unordered_map<char, unsigned>& freqMap) {
     priority_queue<HuffmanNode*, vector<HuffmanNode*>, NodeComparator> minHeap;
     
     
-    for (const auto& pair : freqMap) {
+    for (auto& pair : freqMap)
         minHeap.push(new HuffmanNode(pair.first, pair.second));
-    }
+    
 
     while (minHeap.size() > 1) {
         
@@ -54,7 +51,6 @@ HuffmanNode* buildHuffmanTree(const unordered_map<char, unsigned>& freqMap) {
     return minHeap.top();
 }
 
-// Build Huffman codes
 void buildHuffmanCodes(HuffmanNode* root, const string& code, unordered_map<char, string>& huffmanCodes) {
     if (root == NULL) {
         return;
@@ -62,6 +58,7 @@ void buildHuffmanCodes(HuffmanNode* root, const string& code, unordered_map<char
 
     if (root->data) {
         huffmanCodes[root->data] = code;
+        return;
     }
 
     buildHuffmanCodes(root->left, code + "0", huffmanCodes);
@@ -78,25 +75,23 @@ void huffmanEncode(const string& inputFile, const string& outputFile, unsigned l
     string data((istreambuf_iterator<char>(inFile)), (istreambuf_iterator<char>()));
     unordered_map<char, unsigned> freqMap;
 
-    // Calculate character frequencies
-    for (char c : data) {
+    for (char c : data)
         freqMap[c]++;
-    }
+    
     size = data.length();
     
     HuffmanNode* root = buildHuffmanTree(freqMap);
+    
+    
     
     unordered_map<char, string> huffmanCodes;
     buildHuffmanCodes(root, "", huffmanCodes);
     ::huffmanCodes = huffmanCodes;;
 
-    // Write encoded data to a string
     string encodedData;
-    for (char c : data) {
+    for (char c : data)
         encodedData += huffmanCodes[c];
-    }
 
-    // Write encoded data to the compressed file in 8-bit chunks
     ofstream outFile(outputFile, ios::binary);
     if (!outFile.is_open()) {
         cerr << "Failed to open output file." << endl;
@@ -107,22 +102,24 @@ void huffmanEncode(const string& inputFile, const string& outputFile, unsigned l
     for (size_t i = 0; i < encodedData.length(); i += 8) {
         unsigned char byte = 0;
         string str8 = encodedData.substr(i, 8);
-        for (unsigned b = 0; b < 8; ++b) {
+        for (unsigned b = 0; b < 8; b++) {
             if (b < str8.length()) {
                 byte |= (str8[b] - '0') << b;
             }
         }
         outFile.put(byte);
     }
+    
+    cout << "Successfully compressed..." << endl;
+    long long inSize = inFile.seekg(0, ios::end).tellg(), outSize = outFile.seekp(0, ios::end).tellp();
+    cout << "Input size: " << inSize << endl;
+    cout << "Output size: " << outSize << endl;
+    cout << "A compressed rate of: " << ( (float)outSize/inSize ) * 100 << "%" << endl;
 
-    // Close input and output files
     inFile.close();
     outFile.close();
 }
 
-
-
-// Decode the Huffman-encoded data
 void huffmanDecode(const string& inputFile, const string& outputFile, unsigned long &size) {
     ifstream inFile(inputFile, ios::binary);
     if (!inFile.is_open()) {
@@ -130,9 +127,7 @@ void huffmanDecode(const string& inputFile, const string& outputFile, unsigned l
         return;
     }
 
-    // Read the encoded data and decode it
-
-    ofstream outFile(outputFile, ios::binary);
+    ofstream outFile(outputFile);
     if (!outFile.is_open()) {
         cerr << "Failed to open output file." << endl;
         inFile.close();
@@ -144,18 +139,18 @@ void huffmanDecode(const string& inputFile, const string& outputFile, unsigned l
     string bitStr = "";
     string reverse = "";
     unsigned char buffer;
-    unsigned long origLength = size; // Replace this with the actual original data length --------
+    unsigned long origLength = size;
     
     
     unordered_map<string, char> uMap;
-    unordered_map<char, string> huffmanCodes = :: huffmanCodes;;
     for (const auto& pair : huffmanCodes) {
-        uMap[pair.second] = pair.first; // Reverse mapping: Huffman code to character
+        uMap[pair.second] = pair.first;
     }
 
     while (inFile.read(reinterpret_cast<char*>(&buffer), sizeof(buffer))) {
         bitset<8> setByte(buffer);
         bitStr = setByte.to_string();
+        
         reverse = "";
 
         for (int k = 7; k >= 0; k--) {
@@ -176,28 +171,26 @@ void huffmanDecode(const string& inputFile, const string& outputFile, unsigned l
             }
         }
     }
+    cout << "Successfully decompressed..." << endl;
     
     inFile.close();
     outFile.close();
 }
 
 
-
-
 int main() {
     unsigned long size;
-    string inputFile = "/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/input.txt";
-    // cout << "Enter the path to the input file: ";
-    // getline(cin, inputFile);
+    string inputFile; // = "/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/input.txt";
+    cout << "Enter the path to the input file: ";
+    getline(cin, inputFile);
     
-    string compressedFile = "/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/compressed.bin";
-//    string huffmanTree ="/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/huffmanTree.txt";
-    // cout << "Enter the path to the compressed file: ";
-    // getline(cin, compressedFile);
+    string compressedFile; // = "/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/compressed.bin";
+    cout << "Enter the path to the compressed file: ";
+    getline(cin, compressedFile);
     
-    string decompressedFile = "/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/decompressed.txt";
-    // cout << "Enter the path to the decompressed file: ";
-    // getline(cin, decompressedFile);
+    string decompressedFile; // = "/Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/decompressed.txt";
+    cout << "Enter the path to the decompressed file: ";
+    getline(cin, decompressedFile);
 
     // Just the names
     string inputName = inputFile.substr(inputFile.rfind("/") + 1);
@@ -205,19 +198,16 @@ int main() {
     string decompressedName = decompressedFile.substr(decompressedFile.rfind("/") + 1);
     
     // Compress the file
+    cout << "\nCompressing..." << endl;
     huffmanEncode(inputFile, compressedFile, size);
     cout << inputName << " has been compressed to " << compressedName << endl;
     cout << "Location:" << compressedFile << endl << endl;
     
     // Decompress the file
+    cout << "Decompressing..." << endl;
     huffmanDecode(compressedFile, decompressedFile, size);
     cout << compressedName << " has been decompressed to " << decompressedName << endl;
     cout << "Location:" << decompressedFile << endl;
     
     return 0;
 }
-
-
-// /Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/input.txt
-// /Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/compressed.bin
-// /Users/aadee/Desktop/NMIMS FILES/SEM VII/DSA/Project/File_Compressor/File_Compressor/decompressed.txt
